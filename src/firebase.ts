053fe5fac5edcea51595fe54db7d4870a1a79f71
Andrew McArdle <andrewmcardle1@gmail.com>
2021-02-05 18:07:04 +0000

    basic firebase install

---
+++ src/firebase.ts
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+// firebase init
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDTtC6kF3p22X-Zrf1N87jygKGaPqXR42k",
+    authDomain: "sandbox-41ee6.firebaseapp.com",
+    projectId: "sandbox-41ee6",
+    storageBucket: "sandbox-41ee6.appspot.com",
+    messagingSenderId: "593480959362",
+    appId: "1:593480959362:web:9185cfbe12b79d442ffb9b",
+    measurementId: "G-3GE5GZV51T"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+
+
+// export utils/refs
+export { }
